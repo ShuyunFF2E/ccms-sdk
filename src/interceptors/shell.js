08c11122f4aa4f9906298a805b85173b77ea2068
Kuitos <kuitos.lau@gmail.com>
2016-12-02 19:14:52 +0800

    optimize

--- src/interceptors/shell.js
+++ src/interceptors/shell.js
@@ -7,7 +7,6 @@
 import angular from 'angular';
 
 import tokenRefreshInterceptor, { setAuthFailedBehavior, setRefreshTokenUrl } from './token-refresh-interceptor';
-import tokenRefreshInterceptorJq, {setAuthFailedBehavior as setAuthFailedBehaviorJq, setRefreshTokenUrl as setRefreshTokenUrlJq} from './token-refresh-interceptor-jq';
 import apiRequestPrefixInterceptor, { setApiRequestPrefix } from './api-request-prefix-interceptor';
 import { getRequestCredential, setRequestCredential } from '../credentials';
 
@@ -25,15 +24,3 @@ export default angular
 		setApiRequestPrefix
 	})
 	.name;
-
-// 为es5提供调用
-if (!window.ccmsSdk) window.ccmsSdk = {};
-Object.assign(window.ccmsSdk, {
-	tokenRefreshInterceptor: tokenRefreshInterceptorJq,
-	$ccmsAuth: {
-		setAuthFailedBehavior: setAuthFailedBehaviorJq,
-		setRefreshTokenUrl: setRefreshTokenUrlJq,
-		getRequestCredential,
-		setRequestCredential
-	}
-});
