71fbcfdfea64cbd227dca38faf62aa57ebb27bf3
peach <356127343@qq.com>
2017-07-26 10:55:33 +0800

    get async expires time

--- src/interceptors/metadata.js
+++ src/interceptors/metadata.js
@@ -3,12 +3,16 @@
  * @homepage https://github.com/qixman/
  * @since 2016-10-21
  */
+import { getRequestCredential } from '../credentials';
+
+const credential = getRequestCredential();
 
 export const Date = window.Date;
 export const noop = () => {};
 export const REQUEST_TOKEN_HEADER = 'Authorization';
 export const REQUEST_TOKEN_VALUE = value => `Bearer ${value}`;
-export const USER_SESSION_AVAILABLE_TIME = 30 * 60 * 1000;
+// export const USER_SESSION_AVAILABLE_TIME = 30 * 60 * 1000;
+export const USER_SESSION_AVAILABLE_TIME = (credential && credential['expires_in']) ? credential['expires_in'] * 1000 / 2 : 30 * 60 * 1000;
 export const REQUEST_WHITE_LIST = [];
 export const CREDENTIAL_KEY_MAPPER = {
 	accessToken: 'access_token',
